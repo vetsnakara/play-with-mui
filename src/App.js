6fffcaf47caf80f2fb9fbbf0fe3eafae4178d5d1
Konstantin Arakantsev <kostya3312@gmail.com>
2020-02-16 00:27:58 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+import Button from "@material-ui/core/Button";
+
+export default function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
